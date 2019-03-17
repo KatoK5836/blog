0162bbb0373b3b8ff2aa829fb4af4ce1929b3281
EC2 Default User <ec2-user@ip-172-31-43-241.ap-southeast-1.compute.internal>
2019-03-17 11:41:57 +0000

    topコントローラをtopsコントローラに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'top' => 'top#index'
+get 'tops' => 'tops#index'
 
 end
